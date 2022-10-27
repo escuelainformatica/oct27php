03ae999dbb65dadce42508926413a859b552d94e
jorgecc <jcastro@eftec.cl>
2022-10-27 10:02:48 -0300

    2

---
+++ app/Servicios/ProductoRepo.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Servicios;
+
+
+use App\Models\Log;
+use App\Models\Product;
+use App\Models\Purchase;
+use Illuminate\Support\Facades\DB;
+
+class ProductoRepo {
+
+    public function compra(int $idProducto,string $provider, int $cantidad):bool {
+        $compra=new Purchase(['idproduct'=>$idProducto,'provider'=>$provider,'quantity'=>$cantidad]);
+        return $this->compra2($compra);
+    }
+    public function compra2(Purchase $compra):bool {
+        // 1) modificar el stock del producto
+        // 1.1) el producto existe?
+        // 2) agregar un purchase.
+        // 3) registrar la operacion en el archivo de log.
+        DB::transaction(function() use($compra) {
+            // 1)
+            $prod=Product::find($compra->idproduct);
+            if($prod===null) {
+                throw new \Exception('el producto no existe');
+            }
+            $prod->stock += $compra->quantity;
+            $prod->save();
+            // 2)
+            $compra->save();
+            // 3)
+            $log=new Log(['event'=>'se compro un producto']);
+            $log->save();
+        });
+        return true;
+    }
+}
---
+++ app/Servicios/ProductoSrv.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Servicios;
+
+// clase de servicio es una coleccion de funciones.
+
+class ProductoSrv {
+
+}
